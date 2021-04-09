7b1d1b8459ca63ae762f8a2c6c2db1374d2f5b83
Glenn Sandvoss <gsandvoss@gmail.com>
2021-04-09 06:44:00 -0700

    added functionality to get air quality reading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,32 @@ class HomeController < ApplicationController
   	require 'net/http'
   	require 'json'
 
-
-  	@url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=~zipCode~&distance=25&API_KEY=A66CD3ED-A8F4-4584-BEC8-9D24DA26A6E6'
+  	@url = 'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=99205&distance=25&API_KEY=A66CD3ED-A8F4-4584-BEC8-9D24DA26A6E6'
   	@uri = URI(@url)
   	@response = Net::HTTP.get(@uri)
   	@output = JSON.parse(@response)
+  	#check for empty return results
+  	if @output.empty?
+  		@final_output = "Error"
+  	else
+  		@final_output = @output [0]['AQI']
+  	end
+
+  	if @final_output == "Error"
+  		@api_color = "grey"
+  	elsif @final_output <= 50
+  		@api_color = "green"
+  	elsif @final_output >= 51 && @final_output <= 100
+  		@api_color = "yellow"
+   	elsif @final_output >= 101 && @final_output <= 150
+  		@api_color = "orange"
+   	elsif @final_output >= 151 && @final_output <= 200
+  		@api_color = "red"
+   	elsif @final_output >= 201 && @final_output <= 300
+  		@api_color = "purple"
+  	elsif @final_output >= 301 && @final_output <= 500
+  		@api_color = "maroon"
+  		
+  	end
   end
 end

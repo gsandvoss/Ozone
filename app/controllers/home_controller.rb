a1ea87f26c4d72f29edcfd50a8cd81d26beb23a6
Glenn Sandvoss <gsandvoss@gmail.com>
2021-04-10 10:22:14 -0700

    added descriptions and changed font size and color

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,22 +13,27 @@ class HomeController < ApplicationController
   	else
   		@final_output = @output [0]['AQI']
   	end
-
+  	#<h1 class="display-4"><%= @final_output%></h1>
   	if @final_output == "Error"
   		@api_color = "grey"
   	elsif @final_output <= 50
   		@api_color = "green"
+  		@api_description = "Good, go outside and breathe!"
   	elsif @final_output >= 51 && @final_output <= 100
   		@api_color = "yellow"
+  		@api_description = "Moderate, it's not too bad."
    	elsif @final_output >= 101 && @final_output <= 150
   		@api_color = "orange"
+  		@api_description = "Unhealthy for Sensitive Groups"
    	elsif @final_output >= 151 && @final_output <= 200
   		@api_color = "red"
+  		@api_description = "Unhealthy"
    	elsif @final_output >= 201 && @final_output <= 300
   		@api_color = "purple"
+  		@api_description = "Very Unhealthy"
   	elsif @final_output >= 301 && @final_output <= 500
   		@api_color = "maroon"
-  		
+  		@api_description = "Hazardous"
   	end
   end
 end
